991649c0894b9ed485f18354d45dcd6999e2fb42
Mark <sitesensedev@gmail.com>
2015-02-18 21:51:37 +0000

    Update reset.blade.php
    Minor changes to apply focus to inputs when labels are clicked.

--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -23,23 +23,23 @@
 						<input type="hidden" name="token" value="{{ $token }}">
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
+							<label for="email" class="col-md-4 control-label">E-Mail Address</label>
 							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
+								<input type="email" id="email" class="form-control" name="email" value="{{ old('email') }}">
 							</div>
 						</div>
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
+							<label for="password" class="col-md-4 control-label">Password</label>
 							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
+								<input type="password" id="password" class="form-control" name="password">
 							</div>
 						</div>
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">Confirm Password</label>
+							<label for="password_confirmation" class="col-md-4 control-label">Confirm Password</label>
 							<div class="col-md-6">
-								<input type="password" class="form-control" name="password_confirmation">
+								<input type="password" id="password_confirmation" class="form-control" name="password_confirmation">
 							</div>
 						</div>
 
